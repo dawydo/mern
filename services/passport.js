140936ed38204f0ec322773f084146c872c4912d
Dawydo <davelavi@gmail.com>
2020-02-12 02:03:21 +0100

    Changed callbackURL

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'https://fathomless-dawn-15949.herokuapp.com/auth/google/callback',
+        callbackURL: '/auth/google/callback',
         proxy: true
     }, 
     (accessToken, refreshToken, profile, done) => {
