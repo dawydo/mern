e5e385fd6e7700b7ef60e500062b929a0fe0dc22
Dawydo <davelavi@gmail.com>
2020-02-23 00:42:40 +0100

    Made new changes to prodjs

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,18 +1,17 @@
 //Install npm stripe - see stripe api docs for Charges
 //Install npm install --save body-parser
-const keys = require('../config/keys')
-const stripe = require('stripe')('sk_test_b2jfolM9twbcIyhA8pHFQ1Nr00UKknnbDW')//(keys.stripeSecretKey)
-const requireLogin = require('../middlewares/requireLogin')
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
  
- module.exports = app => {
-     app.post('/api/stripe', requireLogin, async (req, res) => {
-
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: '$5 for 5 credits',
-            source: req.body.id
-        })
+module.exports = app => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+      const charge = await stripe.charges.create({
+        amount: 500,
+        currency: 'usd',
+        description: '$5 for 5 credits',
+        source: req.body.id
+      });
 
         //Using passport initialize and sessions
         //Update credit what user has
